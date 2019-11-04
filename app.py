836b0d82fabc9506c6644baf4c2270d6d9560737
Abdelhadi Dyouri <raryat@gmail.com>
2019-11-04 16:37:55 +0100

    Step 5

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+import sqlite3
 from flask import Flask, render_template
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
